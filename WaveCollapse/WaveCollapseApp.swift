8215538e7a60066905db864b38de2c1c502ad886
David Rynn <davidrynn@gmail.com>
2023-03-25 00:59:44 -0400

    Initial Commit

---
+++ WaveCollapse/WaveCollapseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveCollapseApp.swift
+//  WaveCollapse
+//
+//  Created by David Rynn on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WaveCollapseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
